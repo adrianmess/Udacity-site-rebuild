13ddb77710afb094be9ebf067579b2fcf1bc7dbe
Adrian Messado <adrian.messado@gmail.com>
2018-05-08 00:10:26 -0700

    add footer icons and text

---
+++ src/app/footer-social/footer-social.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-social',
+  templateUrl: './footer-social.component.html',
+  styleUrls: ['./footer-social.component.css']
+})
+export class FooterSocialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
