6a3417ecdd74cccd61350a994d886ce1e0463ac0
Adrian Messado <adrian.messado@gmail.com>
2018-05-10 20:58:40 -0700

    add api key for firebase database

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+    apiKey: "AIzaSyAEZ55TmDv6ljyZBAL49qprn9vFJlKIiGQ",
+    authDomain: "udacity-site-rebuild.firebaseapp.com",
+    databaseURL: "https://udacity-site-rebuild.firebaseio.com",
+    projectId: "udacity-site-rebuild",
+    storageBucket:  "",
+    messagingSenderId: "943261677910"
+  };
