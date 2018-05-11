1b48a763c66799c5d68f55c37946de2778db512e
Adrian Messado <adrian.messado@gmail.com>
2018-05-10 22:23:36 -0700

    add import statement for form, formService to admin component. add constructor to AdminComponent class

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Form } from '../form.model';
+import { FormService } from '../form.service';
+
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [FormService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formService: FormService) { }
 
   ngOnInit() {
+  }
+
+  submitForm(title: string, artist: string, description: string) {
+    var newForm: Form = new Form(title, artist, description);
+    this.formService.addForm(newForm);
   }
 
 }
