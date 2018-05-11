9a14c17d3cc8889f898e8dab692a8e9785ca476e
Adrian Messado <adrian.messado@gmail.com>
2018-05-10 22:43:13 -0700

    admin.component: import router, add formservice provider, add FirebaseListObservable, create constructor, add gotdetailpage and submit form method

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { Form } from '../form.model';
 import { FormService } from '../form.service';
 
+import { Router } from '@angular/router';
+//Ability to List forms
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-admin',
@@ -12,10 +15,23 @@ import { FormService } from '../form.service';
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private formService: FormService) { }
+//<----------for listing forms------------>
+forms: FirebaseListObservable<any[]>;
+currentRoute: string = this.router.url;
+
+  constructor(private router: Router, private formService: FormService) { }
 
   ngOnInit() {
+    this.forms = this.formService.getForms();
+    // console.log(this.router.url); --display url route in console
   }
+
+
+// ----------------Likely won't be used but added just in case-----------------
+  goToDetailPage(clickedForm) {
+    this.router.navigate(['forms', clickedForm.$key]);
+  }
+// --------------------------------------------------------------------------------
 
   submitForm(title: string, artist: string, description: string) {
     var newForm: Form = new Form(title, artist, description);
