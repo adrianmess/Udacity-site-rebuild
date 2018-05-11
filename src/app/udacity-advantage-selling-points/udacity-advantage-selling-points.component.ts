636bfee063e716634a9faa011b796986ac01d28d
Adrian Messado <adrian.messado@gmail.com>
2018-05-11 12:58:53 -0700

    add selling points component

---
+++ src/app/udacity-advantage-selling-points/udacity-advantage-selling-points.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-udacity-advantage-selling-points',
+  templateUrl: './udacity-advantage-selling-points.component.html',
+  styleUrls: ['./udacity-advantage-selling-points.component.css']
+})
+export class UdacityAdvantageSellingPointsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
