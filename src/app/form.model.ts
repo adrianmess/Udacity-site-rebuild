e97730be9c33bd8f35d30d6392eddcec913a41fe
Adrian Messado <adrian.messado@gmail.com>
2018-05-16 12:56:49 -0700

    remove commented out code from form model, adjust footer html indentation

--- src/app/form.model.ts
+++ src/app/form.model.ts
@@ -1,7 +1,3 @@
-// export class Form {
-//   constructor (public title: string, public artist: string, public description: string) { }
-// }
-
 export class Form {
   constructor (public Links: any) { }
 }
