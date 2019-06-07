004bc349fb95b575d6dc9ca4a9de1f3af1d4cca4
Adrian Messado <adrian.messado@gmail.com>
2019-06-06 21:46:11 -0700

    update form model to match cards array in firebase

--- src/app/form.model.ts
+++ src/app/form.model.ts
@@ -1,3 +1,3 @@
 export class Form {
-  constructor (public FProgram: string, public Oudacity: string, public Resources: string, public Programs: string, public Company: string, public Business: string, public Support: string) { }
+  constructor(public title: string, public bgImgUrl: string, public pillTxt: string, public h4text: string, public divId: string) { }
 }
