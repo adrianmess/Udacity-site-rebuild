2d1e8e939f3019a1fb65dd935805869e312dbf5b
Adrian Messado <adrian.messado@gmail.com>
2019-06-06 21:33:45 -0700

    remove import of Router, Form, FormService & FirebaseListObservable. Remove FormService provider. Remove Router and Formservice from constructor

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,24 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Form } from '../form.model';
-import { Router } from '@angular/router';
-import { FormService } from '../form.service';
-import { FirebaseListObservable } from 'angularfire2/database';
+
 
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
-  providers: [FormService]
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
-  forms: FirebaseListObservable<any[]>;
-  currentRoute: string = this.router.url;
-  constructor(private router: Router, private formService: FormService) { }
 
-  ngOnInit() {
-    this.forms = this.formService.getForms();
-  }
+  constructor() { }
+
+  ngOnInit() {}
 
 }
