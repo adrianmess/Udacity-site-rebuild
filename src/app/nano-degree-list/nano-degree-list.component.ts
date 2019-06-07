11ce444edd22773f4c4978165ee497b6796ce8c1
Adrian Messado <adrian.messado@gmail.com>
2019-06-06 20:23:00 -0700

    set forms object to FirebaseListObservable, set currentRoute object

--- src/app/nano-degree-list/nano-degree-list.component.ts
+++ src/app/nano-degree-list/nano-degree-list.component.ts
@@ -7,10 +7,13 @@ import { FirebaseListObservable } from 'angularfire2/database';
 @Component({
   selector: 'app-nano-degree-list',
   templateUrl: './nano-degree-list.component.html',
-  styleUrls: ['./nano-degree-list.component.css']
+  styleUrls: ['./nano-degree-list.component.css'],
+  providers: [FormService]
 })
 export class NanoDegreeListComponent implements OnInit {
 
+  forms: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
   constructor() { }
 
   ngOnInit() {
