05acbfc491fb8c60fa68b54a5aae52a13f26c3ec
Adrian Messado <adrian.messado@gmail.com>
2019-06-09 10:07:23 -0700

    import cards json file which contains default list of nano-degree-cards, set root to FirebaseList class, create resetForm method to push cards object to firebase

--- src/app/formreset.service.ts
+++ src/app/formreset.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+// import { Form } from './form.model';
+import {cards} from './default-cards';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database'
+
+
 
 @Injectable()
 export class FormresetService {
 
-  constructor() { }
+  root: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.root = database.list('/');
+}
+
+
+resetForm(cards) {
+    this.root.push(cards);
+  }
 
 }
